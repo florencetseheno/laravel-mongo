4a546051580114f52e9deef868820381e96c6dbc
florencetseheno <florencetseheno@gmail.com>
2022-06-08 16:40:54 +0200

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function frontend(){
+        return product::all();
+    }
+    public function backend(Request $request){
+        $query=product::query();
+        if($s=$request->input(key:'S')){
+            $query->where('title','regexp',"/$s/")
+                ->orWhere('description','regexp',"/$s/");
+
+        }
+        return $query->get();
+    }
+}
